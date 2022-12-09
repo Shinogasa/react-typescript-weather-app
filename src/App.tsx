cd7e6244e82cd0f80df95931942b3b1897c15c35
Shinonome <cuulbulu@gmail.com>
2022-12-09 17:44:50 +0900

    Update: Formの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Title from './components/title';
 import './App.css';
+import Title from './components/title';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="test">
       <Title />
+      <Form />
     </div>
   );
 }
