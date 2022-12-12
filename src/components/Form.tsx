b2a46cf920f803a6ed305002654feae44fc1c796
Shinonome <cuulbulu@gmail.com>
2022-12-12 17:28:27 +0900

    Update: apiを使ってロンドンの天気を取得

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 
 const Form = () => {
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState<string>("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=277a2c6b88a3418390e72732221212&q=London&aqi=no")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    };
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={ e=>setCity(e.target.value) }/>
-            {city}
-            <button type="submit">GetWeather</button>
+            <button type="submit" onClick={getWeather}>GetWeather</button>
         </form>
     );
 };
