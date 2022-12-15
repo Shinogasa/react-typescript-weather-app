b5fd3d781734966f4f990a62189dae2221c66d30
Shinonome <cuulbulu@gmail.com>
2022-12-15 14:55:06 +0900

    Update: 2章

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react';
+type FormPropsType = {
+    setCity: React.Dispatch<React.SetStateAction<string>>;
+    getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
+}
 
-const Form = () => {
-    const [city, setCity] = useState<string>("");
-    const getWeather = (e: any) => {
-        e.preventDefault();
-        fetch("http://api.weatherapi.com/v1/current.json?key=277a2c6b88a3418390e72732221212&q=London&aqi=no")
-            .then(res => res.json())
-            .then(data => console.log(data))
-    };
+const Form = (props: FormPropsType) => {
     return (
-        <form>
-            <input type="text" name="city" placeholder="都市名" onChange={ e=>setCity(e.target.value) }/>
-            <button type="submit" onClick={getWeather}>GetWeather</button>
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/> 
+            <button type="submit">Get Weather</button> 
         </form>
     );
 };
